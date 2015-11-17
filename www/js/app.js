b541de075341d49c7fd53fa6b1ea65a16b9cd599
Patrick Kocsis <patrick.kocsis@bgch.co.uk>
2015-11-17 21:42:23 +0100

    working on REST, Getting Austrian Data working , Button oesterreich on Homescreen working

--- www/js/app.js
+++ www/js/app.js
@@ -4,51 +4,57 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','starter.resources'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-      .state('app.home', {
-        url: '/home',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/home.html',
-            controller: 'HomeCtrl'
-          }
-        }
-      })
-      .state('app.bebenDetail', {
-        url: '/bebenDetail/:bebenId',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/beben_detail.html',
-            controller: 'BebenDetailCtrl'
-          }
-        }
-      });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
-});
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+            .state('app.home', {
+                url: '/home',
+                resolve: {
+                     AustrianDataResolved :function(AustrianData){
+                         return AustrianData.promise;
+                     }
+                },
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/home.html',
+                        controller: 'HomeCtrl'
+                    }
+                }
+
+            })
+            .state('app.bebenDetail', {
+                url: '/bebenDetail/:bebenId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/beben_detail.html',
+                        controller: 'BebenDetailCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/home');
+    });
